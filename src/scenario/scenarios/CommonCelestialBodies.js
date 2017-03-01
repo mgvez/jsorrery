34b37dabbc5cf27ebbc5cb32a216b9fdf8e74d1b
Martin Vézina <m.vezina@la-grange.ca>
2017-02-28 20:00:59 -0500

    rem console

--- src/scenario/scenarios/CommonCelestialBodies.js
+++ src/scenario/scenarios/CommonCelestialBodies.js
@@ -345,7 +345,6 @@ export const moon = {
 		if (this.relativeTo !== 'earth') return;
 		//when a sideral day has passed, make sure that the near side is still facing the earth. Since the moon's orbit is heavily disturbed, some imprecision occurs in its orbit, and its duration is not always the same, especially in an incomplete scenario (where there are no sun/planets). Therefore, a correction is brought to the base map rotation, tweened so that is is not jerky.
 		if (time >= this.nextCheck) {
-			console.log('check');
 			this.nextCheck += this.sideralDay;
 			TweenMax.to(this, 2, {
 				baseMapRotation: this.getMapRotation(this.getAngleTo('earth')),

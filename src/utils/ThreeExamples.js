e81a47102d4911a3e89cc3df19d8e89f7d4f6184
Martin Vézina <m.vezina@la-grange.ca>
2017-06-05 17:45:51 -0400

    fix unused files

--- src/utils/ThreeExamples.js
+++ src/utils/ThreeExamples.js
@@ -5,9 +5,6 @@ window.THREE = THREE;
 
 require('three.Projector');
 require('three.OrbitControls');
-require('three/examples/js/SkyShader.js');
-
-export const Sky = window.THREE.Sky;
 
 export const Projector = window.THREE.Projector;
 export const OrbitControls = window.THREE.OrbitControls;
